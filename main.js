b9dc4761e037aab236bbdca00ac155f1e62f1ec7
Dmitriy Bezgodko <daimon.94@mail.ru>
2022-08-19 22:51:33 +0300

    19-08-22(22-51)

--- main.js
+++ main.js
@@ -28,10 +28,19 @@ document.querySelector('.container .item_five .content');
 
 const itemSix =
 document.querySelectorAll('.container .item_six .props__list .props__item');
+
+const itemTitle =
+document.querySelectorAll('.container .content .item__title');
+
+const es6Title = itemTitle[0].innerHTML;
+
+const asynchronousProcessingTitle = itemTitle[4].innerHTML;
+
 listTwo[0].append(itemSix[8], itemSix[9]);
 listThree[0].after(listFive[0]);
-console.log(listThree);
 elFive.append(listThree[0]);
-
-
 itemFour[2].after(itemTwo[3]);
+itemTitle[0].innerHTML = itemTitle[3].innerHTML;
+itemTitle[4].innerHTML = es6Title;
+itemTitle[3].innerHTML = asynchronousProcessingTitle;
+itemTitle[1].innerHTML = 'This и прототипы объектов';
